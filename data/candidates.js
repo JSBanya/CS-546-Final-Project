d9e63ac130faf29f0600e32c763893ae8ecccf84
nburgdorfer <nburgdorfer@gmail.com>
2019-05-13 13:39:03 -0400

    Performing manual merge

--- data/candidates.js
+++ data/candidates.js
@@ -2,50 +2,86 @@ const mongoCollections = require("../data/collections");
 const candidates = mongoCollections.candidates;
 const uuid = require("node-uuid");
 
-let exportedMethods = {
     /**
      * Grabs all candidates in the collection
      * @return candidates A list of all the candidates in the collection
      */
-    async getAllCandidates() {
+    const getAllCandidates = async() => {
 
-    },
+    };
 
     /**
      * Grabs all candidates that match all the given keywords
      * @param keywords A list of all the keywords to find candidates
      * @return candidates A list of candidates that match the keywords
      */
-    async candidateSearch(keywords) {
+    const candidateSearch = async(keywords) => {
 
-    },
+    };
 
     /**
      * Grabs the candidate with the given id
      * @param candidateId The id of the requested candidate
      * @return candidate The candidate matching the given id
      */
-    async getCandidateById(candidateId) {
+    const getCandidateById = async(candidateId) => {
+        if(!candidateId) {
+            throw "ERROR: No candidate id given";
+        }
 
-    },
+        const candidatesCollection = await candidates();
+        const candidate = await candidatesCollection.findOne({ _id: id });
+        if (!candidate) {
+            throw "ERROR: No candidate for given id";
+        }
+
+        return candidate;
+    };
+
+    /**
+     * Grabs the candidate with the given email
+     * @param email The email for the requesting candidate
+     * @return candidate The candidate matching the given email
+     */
+    const getCandidateByEmail = async (email) => {
+        if (!email) {
+            throw "ERROR: No email provided";
+        }
+
+        const candidatesCollection = await candidates();
+        const candidate = await candidatesCollection.findOne({ email: email });
+        if (!candidate) {
+            throw "ERROR: No candidate for given email";
+        }
+    
+        return candidate;
+    };
 
     /**
      * Adds a candidate of the given profile object
      * @param profile The profile object for the new candidate
      * @return added True if the candidate was valid and added; False otherwise
      */
-    async addCandidate(profile) {
+    const addCandidate = async(profile) => {
+        if (!profile) {
+            throw "ERROR: No profile given";
+        }
 
-    },
+        const candidatesCollection = await candidates();
+        const info = await candidatesCollection.insertOne(c);
+        if(info.insertedCount === 0) {
+            throw "ERROR: Unable to add candidate to DB";
+        }
+    };
 
     /**
      * Removes a candidate by the given id
      * @param candidateId The id of teh requested candidate to delete
      * @return removed True if the candidate was removed; False otherwise
      */
-    async removeCandidate(candidateId) {
+    const removeCandidate = async(candidateId) => {
 
-    },
+    };
 
     /**
      * Update a candidate name with the given id and name
@@ -53,9 +89,9 @@ let exportedMethods = {
      * @param newName The new name for the candidate
      * @return updated True if the candidate was updated; False otherwise
      */
-    async updateCandidateName(candidateId, newName) {
+    const updateCandidateName = async(candidateId, newName) => {
 
-    },
+    };
 
     /**
      * Update a candidate biography with the given id and biography
@@ -63,9 +99,9 @@ let exportedMethods = {
      * @param newBio The new biography for the candidate
      * @return updated True if the candidate was updated; False otherwise
      */
-    async updateCandidateBio(candidateId, newBio) {
+    const updateCandidateBio = async(candidateId, newBio) => {
 
-    },
+    };
 
     /**
      * Add the given skills to a candidate
@@ -73,9 +109,9 @@ let exportedMethods = {
      * @param newSkills The new skills for the requested candidate
      * @return updated True if the candidate was updated; False otherwise
      */
-    async addSkills(candidateId, newSkills) {
+    const addSkills = async(candidateId, newSkills) => {
 
-    },
+    };
 
     /**
      * Remove the given skills from a candidate
@@ -83,9 +119,9 @@ let exportedMethods = {
      * @param skills The skills to be removed from the requested candidate
      * @return updated True if the canidate was updated; False otherwise
      */
-    async removeSkills(candidateId, skills) {
+    const removeSkills = async(candidateId, skills) => {
 
-    },
+    };
 
     /**
      * Add the given experiences to a candidate
@@ -93,9 +129,9 @@ let exportedMethods = {
      * @param newExp The new experiences for the requested candidate
      * @return updated True if the candidate was updated; False otherwise
      */
-    async addExp(candidateId, newExp) {
+    const addExp = async(candidateId, newExp) => {
 
-    },
+    };
 
     /**
      * Remove the given experiences from a candidate
@@ -103,9 +139,9 @@ let exportedMethods = {
      * @param exp The experiences to be removed from the requested candidate
      * @return updated True if the canidate was updated; False otherwise
      */
-    async removeExp(candidateId, exp) {
+    const removeExp = async(candidateId, exp) => {
 
-    },
+    };
 
     /**
      * Update a candidate image with the given id and image
@@ -113,9 +149,9 @@ let exportedMethods = {
      * @param newImg The new image for the candidate
      * @return updated True if the candidate was updated; False otherwise
      */
-    async updateCandidateImg(candidateId, newImg) {
+    const updateCandidateImg = async(candidateId, newImg) => {
 
-    },
+    };
 
     /**
      * Sends in application notice to the company that posted the job
@@ -123,9 +159,9 @@ let exportedMethods = {
      * @param jobId The given id for the job that was applied to
      * @return applied True if the application is valid and was sent; False otherwise
      */
-    async applyToJob(candidateId, jobId) {
+    const applyToJob = async(candidateId, jobId) => {
 
-    },
+    };
 
     /**
      * Adds hired job to candidate's information
@@ -133,9 +169,24 @@ let exportedMethods = {
      * @param jobId The given id for the job the candidate was hired for
      * @return hired True if the candidate was successfully hired; False otherwise
      */
-    async hired(candidateId, jobId) {
+    const hired = async(candidateId, jobId) => {
 
-    }
+    };
+
+module.exports = {
+    getAllCandidates,
+    candidateSearch,
+	getCandidateById,
+	addCandidate,
+	getCandidateByEmail,
+	removeCandidate,
+    updateCandidateName,
+    updateCandidateBio,
+    updateCandidateImg,
+    addSkills,
+    removeSkills,
+    addExp,
+    removeExp,
+    applyToJob,
+    hired
 };
-
-module.exports = exportedMethods;
