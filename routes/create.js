4932060407b421f2b4a2ce69eec02e8f951623f8
John Banya <JBanya@stevens.edu>
2019-05-12 22:56:30 -0400

    Improved candidate data checking; Added employer data checking

--- routes/create.js
+++ routes/create.js
@@ -31,6 +31,23 @@ router.post('/candidate', async (req, res) => {
 		return;
 	}
 
+	// Size of fields is greater or less than permitted
+	if(data.candidateFirstName.length > 30
+		|| data.candidateLastName.length > 30
+		|| data.candidateEmail.length > 30
+		|| (!isEmpty(data.candidateBiography) && data.candidateBiography.length > 500)
+		|| data.candidatePassword.length > 30
+		|| data.candidateConfirmPassword.length > 30
+		|| data.candidatePassword.length < 8
+		|| data.candidateConfirmPassword.length < 8
+		|| (!Array.isArray(data.candidateSkill) && !isEmpty(data.candidateSkill) && data.candidateSkill.length > 30)
+		|| (!Array.isArray(data.candidateExperience) && !isEmpty(data.candidateExperience) && data.candidateExperience.length > 50)
+		|| (!Array.isArray(data.experienceDescription) && !isEmpty(data.experienceDescription) && data.experienceDescription.length > 1000))
+	{
+		res.status(400).send("400 - Bad Request (bad size check)");
+		return;
+	}
+
 	// If more than one skills, check for all related fields being arrays
 	if(!((Array.isArray(data.candidateSkill) && Array.isArray(data.candidateSkillYears)) 
 		|| (!Array.isArray(data.candidateSkill) && !Array.isArray(data.candidateSkillYears)))) 
@@ -69,6 +86,25 @@ router.post('/candidate', async (req, res) => {
 		return;
 	}
 
+	// Check for array size restrictions
+	if (Array.isArray(data.candidateSkill)) {
+		for(let i = 0; i < data.candidateSkill.length; i++) {
+			if(data.candidateSkill[i].length > 30) {
+				res.status(400).send("400 - Bad Request (bad skill array size check)");
+				return;
+			}
+		}
+	}
+
+	if (Array.isArray(data.candidateExperience)) {
+		for(let i = 0; i < data.candidateExperience.length; i++) {
+			if(data.candidateExperience[i].length > 50 || data.experienceDescription[i].length > 1000) {
+				res.status(400).send("400 - Bad Request (bad experience array size check)");
+				return;
+			}
+		}
+	}
+
 	// Check for valid numbers of skill years
 	if(Array.isArray(data.candidateSkillYears)) {
 		for(let i = 0; i < data.candidateSkillYears; i++) {
@@ -100,6 +136,46 @@ router.post('/candidate', async (req, res) => {
 	// Check for valid password match
 	if(data.candidatePassword != data.candidateConfirmPassword) {
 		res.status(400).send("400 - Bad Request (invalid password confirmation)");
+		return;
+	}
+
+	// Good to go; Store
+	console.log(data);
+
+	res.redirect("/");
+});
+
+router.post('/employer', async (req, res) => {
+	let data = req.body;
+	let isEmpty = (x) => {
+		if(x === undefined || x === null || x === "") {
+			return true;
+		}
+		return false;
+	};
+
+	/*****************
+	* Validate request
+	******************/
+	if(isEmpty(data.employerName) 
+		|| isEmpty(data.employerEmail) 
+		|| isEmpty(data.employerPassword) 
+		|| isEmpty(data.employerConfirmPassword)) 
+	{
+		// A required field is empty
+		res.status(400).send("400 - Bad Request (empty required field)");
+		return;
+	}
+
+	if(data.employerName.length > 50
+		|| data.employerEmail.length > 30
+		|| data.employerPassword.length > 30
+		|| data.employerConfirmPassword.length > 30
+		|| data.employerPassword.length < 8
+		|| data.employerConfirmPassword.length < 8
+		|| (!isEmpty(data.employerDescription) && !isEmpty(data.employerDescription) && data.employerDescription > 500))
+	{
+		res.status(400).send("400 - Bad Request (bad size check)");
 		return;
 	}
 
