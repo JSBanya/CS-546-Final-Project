64d72c2fe7ad6bb34528c3042354494657ad294e
nburgdorfer <nburgdorfer@gmail.com>
2019-05-13 14:27:48 -0400

    Performing manual merge on messages.js

--- data/messages.js
+++ data/messages.js
@@ -2,7 +2,7 @@ const mongoCollections = require("../data/Collections");
 const messages = mongoCollections.messages;
 const uuid = require("node-uuid");
 
-let exportedMethods = {
+
     /**
      * Messages Object Structure (stand-alone):
      *          { 
@@ -38,49 +38,49 @@ let exportedMethods = {
      * (I do not see a use case for this but I'll keep it anyway)
      * @return messages All messages in the collection
      */
-    async getAllMessages() {
+    const getAllMessages = async() => {
 
-    }
+    };
     
     /**
      * Grabs all the conversations of an employer
      * @param employerId The id for the employer
      * @return conversations The conversations of an employer
      */
-    async getAllEmployerConversations(employerId) {
+    const getAllEmployerConversations = async(employerId) => {
 
-    }
+    };
 
     /**
      * Grabs all the conversations of a candidate
      * @param candidateId The id for the requested candidate
      * @return conversations The conversations of a candidate
      */
-    async getAllCandidateConversations(candidateId) {
+    const getAllCandidateConversations = async(candidateId) => {
 
-    }
+    };
 
     /**
      * Adds a message of the given object to the given candidate
      * @param candidateId The candidate's id of the message
      * @param employerId The employer's id of the message
      * @param newMessage The new message object
-     * @return sent True if the message was sent; False otherwise
+     * @return none Throws an error if the message was not sent
      */
-    async sendMessageToCand(candidateId, employerId, newMessage) {
+    const sendMessageToCand = async(candidateId, employerId, newMessage) => {
 
-    }
+    };
 
     /**
      * Adds a message of the given object to the given employer
      * @param candidateId The candidate's id of the message
      * @param employerId The employer's id of the message
      * @param newMessage The new message object
-     * @return sent True if the message was sent; False otherwise
+     * @return none Throws an error if the message was not sent
      */
-    async sendMessageToEmpl(candidateId, employerId, newMessage) {
+    const sendMessageToEmpl = async(candidateId, employerId, newMessage) => {
 
-    }
+    };
 
     /**
      * Deletes a conversation with the given candidate and employer id 
@@ -88,11 +88,17 @@ let exportedMethods = {
      * @param condidateId The id of the message's candidate
      * @param employerId The id of the message's employer
      * @param whoDeleted A string indicating who is deleting the conversation ("candidate" | "employer")
-     * @return deleted True is the conversation was deleted; False otherwise
+     * @return none Throws an error if the conversation was not deleted
      */
-    async deleteConversation(candidateId, employerId, whoDeleted) {
+    const deleteConversation = async(candidateId, employerId, whoDeleted) => {
 
-    }
+    };
+
+module.exports = {
+    getAllMessages,
+    getAllCandidateConversations,
+    getAllEmployerConversations,
+    sendMessageToCand,
+    sendMessageToEmpl,
+    deleteConversation
 };
-
-module.exports = exportedMethods;
